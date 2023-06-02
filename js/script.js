56ecda819beec271dab54850918c5434bdd15c91
May_Edu <mayck22.03@hotmail.com>
2023-06-02 16:10:58 -0300

    up

--- js/script.js
+++ js/script.js
@@ -13,8 +13,10 @@ let jogadas = 0;
 let final_jogo = false;
 renan = '<img src="img/108248455-removebg-preview.png">'
 cassio = '<img src="img/127757407-removebg-preview.png">'
+velha = '<img src="img/claudemir.jpg">'
 player1 = renan
 player2 = cassio
+
 
 function p1(){
     player1 = '<img src="img/108248455-removebg-preview.png">'
@@ -179,12 +181,6 @@ function clickBtn9() {
 function jogada() {
     jogadas += 1
     verificaJogador()
-    if (jogadas == 9) {
-        final_jogo = true
-        alert("Deu velha")
-        lblJogador.innerText = "Velha"
-    }
-
 }
 
 function verificaJogador() {
@@ -194,54 +190,63 @@ function verificaJogador() {
     if ((btn1 == 'X' && btn2 == 'X' && btn3 == 'X') || (btn4 == 'X' && btn5 == 'X' && btn6 == 'X') || (btn7 == 'X' && btn8 == 'X' && btn9 == 'X')) {
         final_jogo = true
         lblJogador.innerText = 'O Jogador 1 ganhou'
-        y = "X"
+        y = 1
 
     }
-    if ((btn1 == 'X' && btn4 == 'X' && btn7 == 'X') || (btn2 == 'X' && btn5 == 'X' && btn8 == 'X') || (btn3 == 'X' && btn6 == 'X' && btn9 == 'X')) {
+    else if ((btn1 == 'X' && btn4 == 'X' && btn7 == 'X') || (btn2 == 'X' && btn5 == 'X' && btn8 == 'X') || (btn3 == 'X' && btn6 == 'X' && btn9 == 'X')) {
         final_jogo = true
         lblJogador.innerText = 'O Jogador 1 ganhou'
-        y = "X"
+        y = 1
 
     }
-    if ((btn1 == 'X' && btn5 == 'X' && btn9 == 'X') || (btn3 == 'X' && btn5 == 'X' && btn7 == 'X')) {
+    else if ((btn1 == 'X' && btn5 == 'X' && btn9 == 'X') || (btn3 == 'X' && btn5 == 'X' && btn7 == 'X')) {
         final_jogo = true
         lblJogador.innerText = 'O Jogador 1 ganhou'
-        y = "X"
+        y = 1
 
     }
 
 
-    if ((btn1 == 'O' && btn2 == 'O' && btn3 == 'O') || (btn4 == 'O' && btn5 == 'O' && btn6 == 'O') || (btn7 == 'O' && btn8 == 'O' && btn9 == 'O')) {
+    else if ((btn1 == 'O' && btn2 == 'O' && btn3 == 'O') || (btn4 == 'O' && btn5 == 'O' && btn6 == 'O') || (btn7 == 'O' && btn8 == 'O' && btn9 == 'O')) {
         final_jogo = true
         lblJogador.innerText = 'O Jogador 2 ganhou'
-        y = "O"
+        y = 2
 
     }
-    if ((btn1 == 'O' && btn4 == 'O' && btn7 == 'O') || (btn2 == 'O' && btn5 == 'O' && btn8 == 'O') || (btn3 == 'O' && btn6 == 'O' && btn9 == 'O')) {
+    else if ((btn1 == 'O' && btn4 == 'O' && btn7 == 'O') || (btn2 == 'O' && btn5 == 'O' && btn8 == 'O') || (btn3 == 'O' && btn6 == 'O' && btn9 == 'O')) {
         final_jogo = true
         lblJogador.innerText = 'O Jogador 2 ganhou'
-        y = "O"
+        y = 2
 
     }
-    if ((btn1 == 'O' && btn5 == 'O' && btn9 == 'O') || (btn3 == 'O' && btn5 == 'O' && btn7 == 'O')) {
+    else if ((btn1 == 'O' && btn5 == 'O' && btn9 == 'O') || (btn3 == 'O' && btn5 == 'O' && btn7 == 'O')) {
         final_jogo = true
         lblJogador.innerText = 'O Jogador 2 ganhou'
-        y = "O"
+        y = 2
 
+    }else if(jogadas == 9){
+        final_jogo = true
+        alert("Deu velha")
+        lblJogador.innerText = "Velha"
+        y = 3
     }
-    if (final_jogo == true) {
-        var divBtn1 = document.getElementById("linha"); //vc parou aq
-        var imgElement = divBtn1.getElementsByTagName("img")[0];
 
-        // Removendo o elemento img
-        divBtn1.removeChild(imgElement);
-        return
-
-    }
     if (controle == 0) {
         lblJogador.innerText = "Vez de 'O'"
     } else {
         lblJogador.innerText = "Vez de 'X'"
     }
     lblJogadas.innerText = '( jogada ' + (jogadas) + ' de 9' + ')'
+
+    if(final_jogo){
+        if(y == 1){
+            document.getElementById("resultado").innerHTML = player1 + '<h3>Cássio ganhou</h3>'
+        } 
+        if(y == 2){
+            document.getElementById("resultado").innerHTML = player2 + '<h3>Renan ganhou</h3>'
+        }
+        if(y == 3){
+            document.getElementById("resultado").innerHTML = velha + '<h3>Não foi dessa vez</h3>'
+        }
+    }
 }
